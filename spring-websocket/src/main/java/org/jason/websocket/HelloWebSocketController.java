ead3f733c9a376eaa562e1a3a287c91216e2f38c
Jason Zhang <zclok236@gmail.com>
2018-08-26 22:03:09 +1000

    [spring-websocket] - Init project spring websocket from original bitbucket

---
+++ spring-websocket/src/main/java/org/jason/websocket/HelloWebSocketController.java
@@ -0,0 +1,33 @@
+package org.jason.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWebSocketController {
+
+  @Autowired
+  public SimpMessageSendingOperations messagingTemplate;
+  
+  @Autowired
+  public HelloWebSocketController(SimpMessageSendingOperations messagingTemplate) {
+    this.messagingTemplate = messagingTemplate;
+  }
+  
+    @MessageMapping("/hello")
+    @SendTo("/client/hello")
+    public HelloMessage greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new HelloMessage("Hello, " + message.getName() + "!");
+    }
+    
+    @Scheduled(fixedRate=5000)
+    public void sendGreeting() throws Exception {
+      this.messagingTemplate.convertAndSend("/client/hello", new HelloMessage("Hello, " + Math.random()*10 + "!"));
+    }
+
+}
