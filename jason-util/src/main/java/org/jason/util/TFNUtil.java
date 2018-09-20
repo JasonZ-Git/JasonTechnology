96c3db6e0c6be56635e623173b28300abab5d909
Jason Zhang <zclok236@gmail.com>
2018-09-20 19:58:26 +1000

    [TFN Util] - Making the result a string instead of an array

--- jason-util/src/main/java/org/jason/util/TFNUtil.java
+++ jason-util/src/main/java/org/jason/util/TFNUtil.java
@@ -2,7 +2,7 @@ package org.jason.util;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import org.apache.commons.math3.util.MathArrays;
 
@@ -15,17 +15,17 @@ import org.apache.commons.math3.util.MathArrays;
 public final class TFNUtil {
 
 	private static final double[] factor = { 1, 4, 3, 7, 5, 8, 6, 9, 10};
-	
+
 	public static String generateTFN() {
 		
 		int[] randomInts = new Random().ints(9, 1, 10).toArray();
 		
-		if (isValidTFN(randomInts)) return Arrays.toString(randomInts);
+		if (isValidTFN(randomInts)) return Arrays.stream(randomInts).boxed().map(String::valueOf).collect(Collectors.joining());
 		
 		return generateTFN();
 	}
 	
-	
+	;
 	public static boolean isValidTFN(String inputTFN) {
 		
 		return isValidTFN(inputTFN.chars().toArray());
@@ -40,9 +40,4 @@ public final class TFNUtil {
 		return result%11 == 0;
 	}
 	
-	
-	public static void main(String[] args) {
-		
-		IntStream.range(1, 20).mapToObj(i -> generateTFN()).forEach(System.out::println);
-	}
 }
