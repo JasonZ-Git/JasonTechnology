a67a70f7aac62085d53cd1a8595b976e0edca2eb
Jason Zhang <zclok236@gmail.com>
2018-10-30 23:36:26 +1100

    [Code Challenge] - Solution for String compression

---
+++ jason-code-challenge/src/test/java/crack_code_interview_test/Q1_6_StringCompressionTest.java
@@ -0,0 +1,33 @@
+package crack_code_interview_test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import crack_code_interview.Q1_6_String_Compression;
+
+@RunWith(Parameterized.class)
+public class Q1_6_StringCompressionTest {
+  @Parameter(0)
+  public String original;
+  
+  @Parameter(1)
+  public String compressed;
+  
+  @Parameters
+  public static Collection<Object[]> date(){
+    Object[][] data = new Object[][] {{"aabccccaaa","a2b1c4a3"}, {"abcVcDBa","abcVcDBa"}};
+    return Arrays.asList(data);
+  }
+  
+  @Test
+  public void testCompress() {
+    String actual = Q1_6_String_Compression.compress(original);
+    
+    Assert.assertEquals(compressed, actual);
+  }
+}
