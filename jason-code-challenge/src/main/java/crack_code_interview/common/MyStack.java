676baecbc51ac8d205bab4699d724b8eb14b0b12
Jason Zhang <jzhang@bravurasolutions.com>
2018-11-16 17:27:55 +1100

    [Code Challenge] - List & Stack

---
+++ jason-code-challenge/src/main/java/crack_code_interview/common/MyStack.java
@@ -0,0 +1,62 @@
+package crack_code_interview.common;
+
+import java.util.EmptyStackException;
+
+public class MyStack {
+    private int capacity;
+    public Node top;
+    public Node bottom;
+    public int size = 0;
+    
+    public MyStack(int capacity) { 
+        this.capacity = capacity; 
+    }
+    
+    public boolean isFull() {
+        return capacity == size; 
+    }
+    
+    public void join(Node above, Node below) {
+        if (below != null) below.above = above;
+        if (above != null) above.below = below;
+    }
+    
+    public boolean push(int v) {
+        if (size >= capacity) return false;
+        size++;
+        Node n = new Node(v);
+        if (size == 1) bottom = n;
+        join(n, top);
+        top = n;
+        return true;
+    }
+    
+    public int pop() {
+        if (top == null) throw new EmptyStackException();
+        Node t = top;
+        top = top.below;
+        size--;
+        return t.value;
+    }
+    
+    public boolean isEmpty() {
+        return size == 0; 
+    }
+    
+    public int removeBottom() {
+        Node b = bottom;
+        bottom = bottom.above;
+        if (bottom != null) bottom.below = null;
+        size--;
+        return b.value;
+    }
+    
+    public static class Node {
+        public Node above;
+        public Node below;
+        public int value;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
