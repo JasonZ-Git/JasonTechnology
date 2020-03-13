ae49baca7f2aace462cfae32f7d6d38150eacba0
Jason Zhang <zclok236@gmail.com>
2020-03-13 12:19:11 +1100

    [Spring MVC] - Upgrade to Spring 5

--- spring-mvc/src/main/java/org/jason/controller/HelloController.java
+++ spring-mvc/src/main/java/org/jason/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mkyong.common.controller;
+package org.jason.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,7 +12,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 5 MVC Hello World");
 		return "hello";
 
 	}
