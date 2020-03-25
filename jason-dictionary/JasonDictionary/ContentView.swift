216cae1abfa50fa35fbb5b84c328f86bfe796cbc
Jason  Zhang <zclok236@gmail.com>
2020-03-25 22:19:05 +1100

    [Jason Dictionary App] - First Try

---
+++ jason-dictionary/JasonDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JasonDictionary
+//
+//  Created by Jason Zhang on 25/3/20.
+//  Copyright © 2020 Jason Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
