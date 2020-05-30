82bb66b2b04e97b7f1abe328aa63c361f40fe452
Jason  Zhang <zclok236@gmail.com>
2020-05-30 21:41:04 +1000

    [Koala Dictionary] - Add Icon

--- jason-dictionary/Jason Dictionary/TranslationView.swift
+++ jason-dictionary/Jason Dictionary/TranslationView.swift
@@ -7,7 +7,24 @@
 //
 
 import SwiftUI
+import AVFoundation
 
+class SoundPlay {
+    
+    static func play(word : String) {
+        var bombSoundEffect: AVAudioPlayer?
+        let path = Bundle.main.path(forResource: "Sounds/ability", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            //try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            //try AVAudioSession.sharedInstance().setActive(true)
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            print("Something Wrong")
+        }
+    }
+}
 
 class TranslationBinding: ObservableObject {
     
@@ -38,7 +55,7 @@ struct TitleBarImage : View {
 
 struct TranslationView: View {
     
-    @ObservedObject var translation = TranslationBinding()
+    @ObservedObject var wordAndTranslation = TranslationBinding()
 
     var body: some View {
         
@@ -46,14 +63,22 @@ struct TranslationView: View {
             VStack(alignment: .leading) {
                 
                 HStack {
-                    TextField("Word To Go", text: $translation.word)
+                    TextField("Word To Go", text: $wordAndTranslation.word)
                 }
-
-                Text("\(translation.translation)")
+ 
+                Text("\(wordAndTranslation.translation)")
                 Spacer()
                 Text("\(wordDictionary.count) in total")
+                
+                Button(action: {
+                    print("Button Clicked")
+                    SoundPlay.play(word: self.wordAndTranslation.word)
+                }) {
+                    Text("Sound")
+                }
             }
-            .navigationBarTitle("Koala")
+            .navigationBarTitle("考拉词典", displayMode: .inline)
+        
             .padding()
         }
     }
@@ -62,6 +87,7 @@ struct TranslationView: View {
 
 
 struct TranslationView_Previews: PreviewProvider {
+    
     static var previews: some View {
         TranslationView()
     }
