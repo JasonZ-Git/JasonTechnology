4c340deee7abc0f937b81ed95b93dc6dd58077a5
Jason Zhang <zclok236@gmail.com>
2020-07-16 23:18:02 +1000

    [Jason Dictionary] - Add Dockerfile

--- jason-product/jason-dictionary-spider/src/main/java/org.jason.dictionary/DictionarySpiderRestController.java
+++ jason-product/jason-dictionary-spider/src/main/java/org.jason.dictionary/DictionarySpiderRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DictionarySpiderWebController {
+public class DictionarySpiderRestController {
 
     @GetMapping("getGoogleTranslation")
     public String getWordTranslation(@RequestParam(defaultValue = "hello") String word) {
