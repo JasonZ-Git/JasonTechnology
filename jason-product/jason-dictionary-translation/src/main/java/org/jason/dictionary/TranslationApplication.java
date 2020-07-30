f9cff70b0ecc2acd0d18d3c0797aacc4e916f7cf
Jason  Zhang <zclok236@gmail.com>
2020-07-30 23:09:29 +1000

    [Jason Dictionary Translation] - Change port to 10001

--- jason-product/jason-dictionary-translation/src/main/java/org/jason/dictionary/TranslationApplication.java
+++ jason-product/jason-dictionary-translation/src/main/java/org/jason/dictionary/TranslationApplication.java
@@ -3,9 +3,13 @@ package org.jason.dictionary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class TranslationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TranslationApplication.class, args);
+        SpringApplication app = new SpringApplication(TranslationApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "10001"));
+        app.run(args);
     }
 }
