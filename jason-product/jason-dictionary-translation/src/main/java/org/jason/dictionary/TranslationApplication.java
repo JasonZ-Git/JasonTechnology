81ec0779173bcf01c423fa294f65ab5175abe250
Jason Zhang <zclok236@gmail.com>
2020-09-15 21:25:24 +1000

    [Jason Product] - Use application property file

--- jason-product/jason-dictionary-translation/src/main/java/org/jason/dictionary/TranslationApplication.java
+++ jason-product/jason-dictionary-translation/src/main/java/org/jason/dictionary/TranslationApplication.java
@@ -3,27 +3,9 @@ package org.jason.dictionary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class TranslationApplication {
     public static void main(String[] args) {
-
-        SpringApplication app = new SpringApplication(TranslationApplication.class);
-
-        setRunningParameters(args, app);
-
-        app.run(args);
-    }
-
-    private static void setRunningParameters(String[] args, SpringApplication app) {
-
-        if (args != null && args.length !=0 && args[0].equals("dev_mode")) {
-            System.setProperty("mode", "dev");
-            app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
-        } else {
-            System.setProperty("mode", "production");
-            app.setDefaultProperties(Collections.singletonMap("server.port", "10001"));
-        }
+        SpringApplication.run(TranslationApplication.class, args);
     }
 }
