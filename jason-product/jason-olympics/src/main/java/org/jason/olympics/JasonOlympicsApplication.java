8a134a7ade4fdb4bfdcaf205e8325a344b3cbe97
Jason Zhang <zclok236@gmail.com>
2021-08-11 22:42:48 +1000

    [Olympic] - Init Olympic Version

---
+++ jason-product/jason-olympics/src/main/java/org/jason/olympics/JasonOlympicsApplication.java
@@ -0,0 +1,12 @@
+package org.jason.olympics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasonOlympicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasonOlympicsApplication.class, args);
+	}
+}
---
+++ jason-product/jason-olympics/src/test/java/org/jason/olympics/JasonOlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jason.olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasonOlympicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
