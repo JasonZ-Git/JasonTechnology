94bad3181ce00619e32bcb6e816cc4896e1a6ff5
Jason Zhang <zclok236@gmail.com>
2022-01-02 17:36:15 +1100

    [AWS] - Lambda - Working with console - due to eclipse plugin, it is not working with java 11, so have to use console

--- playground/hello-aws-lambda/src/main/java/org/jason/aws/lambda/LambdaFunctionHandler.java
+++ playground/hello-aws-lambda/src/main/java/org/jason/aws/lambda/LambdaFunctionHandler.java
@@ -2,11 +2,13 @@ package org.jason.aws.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+public class LambdaFunctionHandler implements RequestHandler<APIGatewayV2HTTPEvent, String> {
 
   @Override
-  public String handleRequest(Object input, Context context) {
-    return "This is: " + input;
+  public String handleRequest(APIGatewayV2HTTPEvent input, Context context) {
+
+    return "This is: " + input.getRawQueryString();
   }
 }
